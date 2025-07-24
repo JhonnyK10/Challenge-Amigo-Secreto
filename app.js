334eb479f54fe6d6d3de3e48dd2fa1f499dab6af
JhonnyK10 <jhonnylino@gmail.com>
2025-07-24 11:20:37 -0300

    funcao adicionarAmigos

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 let amigos = []
+
+function adicionarAmigo() {
+    let campo = document.getElementById("amigo");
+    let nome = campo.value;
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    amigos.push(nome);
+    campo.value = "";
+}
